9a397a1a9a609babd7af56cfe8f09af6d8762bff
abrahamkojo <kojo123ab@gmail.com>
2022-09-08 10:41:02 -0700

    re check with betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
+
+
 /**
- * main - Entry poin 
- * Return: Always 0 (Success) 
- */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
+
 {
 
-puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+
+
+			return (0);
+
 }
+
+
