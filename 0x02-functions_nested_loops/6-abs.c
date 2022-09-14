d379a08bb113650b8d18a3bbbc01976504c66883
abrahamkojo <kojo123ab@gmail.com>
2022-09-13 20:08:32 -0700

    nuber 6 checked

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints 1 or 0 depending on input
+ * @n: The integer to be computerd.
+ *
+ * Return: The absolute value of the integer..
+ */
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
