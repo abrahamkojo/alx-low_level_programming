550296ef0cfedc666c57890f98c21cc4aa1201a7
unknown <kojo123ab@gmail.com>
2022-09-15 13:58:20 +0000

    work on the advance work

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+/*
+ * File: 101-print_number.c
+ * Auth: Abraham kwadwo Adusei
+ */
+
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ * 
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+}
