e9f762cb1532a9da561f6e4c8cec912fa7f3320c
abrahamkojo <kojo123ab@gmail.com>
2022-09-21 13:27:05 -0700

    remove afile

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,12 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *strcat(char *dest, char *src)
+char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+int index = 0, dest_len = 0;
+while (dest[index++])
+dest_len++;
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
+return (dest);
 }
