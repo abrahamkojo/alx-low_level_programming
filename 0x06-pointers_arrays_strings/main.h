57cd9574b586bc0e3ff4cba83de6d1580b8a0c54
abrahamkojo <kojo123ab@gmail.com>
2022-09-22 08:57:49 -0700

    add the lib

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+
 int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char*, char*, int);
@@ -11,3 +15,4 @@ char *rot13(char*);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+#endif
