e54ae041729a9b87d48267ff9705fe441c5cc0d2
abrahamkojo <kojo123ab@gmail.com>
2022-09-22 09:08:49 -0700

    added a c to the main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stddef.h>
 
-int _putchar(char);
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char*, char*, int);
 char *_strncpy(char*, char*, int);
